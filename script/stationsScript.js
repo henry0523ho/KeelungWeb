e298d6f61447832792dbcf604bb794e1c5e146a2
henry0523ho <80961659+henry0523ho@users.noreply.github.com>
2022-08-10 17:36:17 +0800

    20220810

--- script/stationsScript.js
+++ script/stationsScript.js
@@ -10,6 +10,7 @@ $(function() {
 
 function init() {
     console.log(window.innerWidth, window.innerHeight);
+    $('title').text(jsonData.title);
     initTitle();
     initMain();
     initNav();
@@ -34,7 +35,6 @@ function init() {
 function updateWindow() {
     if ($(window).width() <= 600) {
         console.log("cellphone")
-        $('.stSection').css('display', 'block').css('transform', '');
     } else {}
 }
 
@@ -45,10 +45,7 @@ function initTitle() {
 function initMain() {
     $('main').empty();
     for (let i = 0; i < jsonData.station.length; ++i) {
-        let newSection = generateSection(i);
-        newSection.hide();
-        if (i == 0) newSection.show();
-        $('main').append(newSection);
+        initSection(i);
     }
 }
 
@@ -58,88 +55,158 @@ function initNav() {
         let newStMapName = $("<div></div>")
             .addClass("clickable stMapName")
             .attr("id", ("stMapName" + i))
-            .html(data.name + "<br><span style='font-size:0.5em;'>english name</span>")
+            .html(data.name)
             .click(function() { $.fn.toStation(i); });
         let newStMapNum = $("<div></div>")
             .addClass(i == 0 ? "clickable stMapNum stMapNumOn" : "clickable stMapNum")
             .attr("id", ("stMapNum" + i))
-            .html("KL<br>" + $.fn.fixDigits(i + 1, 2))
+            .html($.fn.fixDigits(i + 1, 2))
             .click(function() { $.fn.toStation(i); });
         let newStMapPath = $("<div></div>")
-            .addClass("stMapPath")
-            .css("width", (i == jsonData.station.length - 1) ? "0px" : "calc(100% + 0px)");
+            .addClass("stMapPath");
         let newStMap = $("<div></div>")
             .addClass("stMap")
             .attr("id", ("stMap" + i))
             .css("width", "calc((100% - 55px)/" + (jsonData.station.length) + ")")
             .append(newStMapName)
-            .append(newStMapNum)
-            .append(newStMapPath);
+            .append(newStMapNum);
+        if (i != jsonData.station.length - 1)
+            newStMap.append(newStMapPath);
         $('nav').append(newStMap);
     });
     $("<div></div>")
         .addClass("stMap")
-        .attr("id", ("stMapStart"))
-        .append(
-            $("<div></div>")
-            .addClass("stMapName")
-            .html("_" + "<br><span style='font-size:0.5em;'>_</span>")
-            .css("color", "transparent"))
+        .attr("id", "stMapStart")
         .append(
             $("<div></div>")
             .addClass("stMapNum stMapNumOn")
-            .attr("id", "stMapNumStart")
-            .html("KL")
-            .css("font-size", "13px")
-            .css("padding-top", "6px"))
-        .append($("<div></div>")
-            .addClass("stMapPath")
-            .css("width", "0px")
-            .css("left", "7px"))
+            .attr("id", "stMapNumSt")
+            .html("起"))
         .insertBefore("#stMap0");
     $("<div></div>")
         .addClass("stMapNum stMapNumOn")
         .attr("id", "stMapNumEnd")
-        .html("KL")
-        .css("margin-left", "1vw")
-        .css("font-size", "13px")
-        .css("padding-top", "6px")
+        .html("終")
         .insertAfter("#stMapNum" + (jsonData.station.length - 1));
 
 }
 
-function generateSection(idx) {
-    let newSection = $('<section></section>');
-    newSection.attr("id", ("stSection" + idx));
-    newSection.attr("class", "stSection");
-    newSection.append(generateName(idx));
-    newSection.append(generateImage(idx));
-    newSection.append(generateInfo(idx));
-    return newSection;
+function initSection(idx) {
+    $('main').append(
+        $('<section></section>')
+        .attr("id", "stSection" + idx)
+        .attr("class", "stSection")
+        .hide()
+    )
+    initName(idx);
+    initSlide(idx);
+    if (idx == 0) {
+        $("#stSection" + idx).show();
+    }
 }
 
-function generateName(idx) {
-    let newName = $('<h1></h1>');
-    newName.attr("id", ("stName" + idx));
-    newName.attr("class", "stName");
-    newName.text(jsonData.station[idx].name);
-    return newName;
+function initSlide(idx) {
+    $("#stSection" + idx).append(
+        $('<div></div>')
+        .attr("class", "slideWin")
+        .attr("id", "slideWin" + idx)
+        .append(
+            $('<div></div>')
+            .attr("class", "slideCon")
+            .attr("id", "slideCon" + idx)
+        )
+        .append(
+            $('<div></div>')
+            .attr("class", "slideNav")
+            .attr("id", "slideNav" + idx)
+        ).scroll(function(e) {
+            $.fn.changeSlide(idx);
+        })
+    );
+    for (let i = 0; i < jsonData.station[idx].info.length; ++i) {
+        initSlideInfo(idx, i);
+        initSlideNav(idx, i);
+    }
 }
 
-function generateImage(idx) {
-    let newImg = $('<img>');
-    newImg.attr("src", ("src/" + jsonData.station[idx].image));
-    newImg.attr("id", ("stImage" + idx));
-    newImg.attr("class", "stImage");
-    return newImg;
+
+function initSlideInfo(stIdx, idx) {
+    $("#slideCon" + stIdx).append(
+        $("<div></div>")
+        .attr("id", "slIf" + stIdx + "_" + idx)
+        .attr("class", "slideInfo")
+        .append(
+            $('<div></div>')
+            .attr("class", "stContent")
+            .append(
+                $('<div></div>')
+                .attr("class", "stPhoto")
+                .append(
+                    $('<img>')
+                    .attr("src", "src/" + jsonData.station[stIdx].info[idx].image)
+                    .attr("class", "stImg")
+                )
+            )
+            .append(
+                $('<div></div>')
+                .attr("class", "stText")
+                .html(jsonData.station[stIdx].info[idx].text)
+            )
+        )
+    )
 }
 
-function generateInfo(idx) {
-    let newInfo = $('<p></p>');
-    newInfo.attr("id", ("stInfo" + idx));
-    newInfo.attr("class", "stInfo");
-    newInfo.text(jsonData.station[idx].info);
-    return newInfo;
+
+function initSlideNav(stIdx, idx) {
+    $("#slideNav" + stIdx).append(
+        $('<div></div>')
+        .attr("class", "slNvNum")
+        .attr("id", "slNvNum" + stIdx + "_" + idx)
+        .text(idx + 1)
+        .css("opacity", "0.3")
+    );
+    if (idx == 0) {
+        $("#slNvNum" + stIdx + "_" + idx).css("opacity", "1");
+    }
+    $("#slNvNum" + stIdx + "_" + idx).click(function() {
+        $.fn.toSlide(stIdx, idx);
+    });
+}
+
+
+function initName(idx) {
+    $("#stSection" + idx).append(
+        $('<h1></h1>')
+        .attr("id", ("stName" + idx))
+        .attr("class", "stName")
+        .text(jsonData.station[idx].name)
+    );
+}
+
+$.fn.changeSlide = function(stIdx) {
+    let w = $("#slIf" + stIdx + "_0")[0].getBoundingClientRect().width;
+    let idx = Math.round($("#slideWin" + stIdx).scrollLeft() / w);
+    $(".slNvNum").each(function() {
+        if ($(this).attr("id") == ("slNvNum" + stIdx + "_" + idx)) {
+            $(this).css("opacity", "1");
+        } else {
+            $(this).css("opacity", "0.3");
+        }
+    });
+}
+
+$.fn.toSlide = function(stIdx, idx) {
+    $("#slideWin" + stIdx)
+        .scrollLeft(
+            $("#slIf" + stIdx + "_" + idx)[0].getBoundingClientRect().width * idx
+        );
+    // $(".slNvNum").each(function() {
+    //     if ($(this).attr("id") == ("slNvNum" + stIdx + "_" + idx)) {
+    //         $(this).css("opacity", "1");
+    //     } else {
+    //         $(this).css("opacity", "0.3");
+    //     }
+    // });
 }
 
 $.fn.toStation = function(n) {
@@ -151,43 +218,11 @@ $.fn.toStation = function(n) {
     $("#stSection" + n).show();
     $("#stSection" + station).hide();
     station = n;
-
+    // $(window).scrollTop(0);
+    $(window)[0].scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+    // $.fn.toSlide(n,0);
+    $.fn.changeSlide(n);
 };
-
-function nextStation() {
-    if (station == jsonData.station.length - 1) return;
-    ++station;
-    $('#stationUl').animate({ left: '+=-20vw' });
-    $('.stSection').each(function() {
-            $(this).removeClass("in inR out outR");
-            $(this).hide();
-            if ($(this).attr("id").replace("stSection", "") == station) {
-                $(this).show();
-                // $(this).addClass("in");
-            } else if ($(this).attr("id").replace("stSection", "") == station - 1) {
-                // $(this).addClass("out");
-            }
-        })
-        // $(('#stSection' + station)).removeClass('in inR out outR').toggleClass("in");
-        // $(('#stSection' + (station - 1))).removeClass('in inR out outR').toggleClass("out");
-
-}
-
-function prevStation() {
-    if (station == 0) return;
-    --station;
-    $('#stationUl').animate({ left: '+=20vw' });
-    $('.stSection').each(function() {
-            $(this).hide();
-            $(this).removeClass("in inR out outR");
-            if ($(this).attr("id").replace("stSection", "") == station) {
-                // $(this).addClass("outR");
-                $(this).show();
-            } else if ($(this).attr("id").replace("stSection", "") == station + 1) {
-                // $(this).addClass("inR");
-            }
-        })
-        // $(('#stSection' + station)).removeClass('in inR out outR').toggleClass("outR");
-        // $(('#stSection' + (station + 1))).removeClass('in inR out outR').toggleClass("inR");
-
-}
