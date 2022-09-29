f22736851154a68c6cd40fdd4e6f664d20a26583
henry0523ho <80961659+henry0523ho@users.noreply.github.com>
2022-09-29 17:14:36 +0800

    20220929

--- script/indexScript.js
+++ script/indexScript.js
@@ -1,11 +1,11 @@
 $(function() {
     $("#hepingBtn").click(function() {
         window.localStorage.setItem("region", "heping");
-        window.location.href = "stations.html";
+        window.location.href = "station.html";
     });
 
     $("#zhengbinBtn").click(function() {
         window.localStorage.setItem("region", "zhengbin");
-        window.location.href = "stations.html";
+        window.location.href = "station.html";
     });
 });
