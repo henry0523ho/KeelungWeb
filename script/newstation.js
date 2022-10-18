a700c95fdb3086507c37ce75dd5bff3a5f18df07
henry0523ho <henry0523ho.3@gmail.com>
2022-10-19 07:58:49 +0800

    test.html

--- script/newstation.js
+++ script/newstation.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
-    $("#collapse").on("click", function() {
-        $("#sidebar").toggleClass("active");
-        $(".fa-align-left").toggleClass("fa-chevron-circle-right");
-    })
-})
+    for (let i = 1; i <= 12; ++i) {
+        $("#navBtn" + i).children("a").click(function() {
+            toStation(i);
+        });
+    }
+    $("#linkMain0").click(function() {
+        toStation(0);
+    });
+
+});
+
+function toStation(idx) {
+    for (let i = 1; i <= 12; ++i) {
+        $("#navBtn" + i).children("a").removeClass("active");
+    }
+    for (let i = 0; i <= 12; ++i) {
+        $("#main" + i).addClass("d-none");
+    }
+    if (idx != 0) {
+        $("#navBtn" + idx).children("a").addClass("active");
+    }
+    $("#main" + idx).removeClass("d-none");
+    $("#sidebarMenu").removeClass("show");
+}
